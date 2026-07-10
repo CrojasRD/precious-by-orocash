7e2f4fa575242903e38d5eaceaccc37fd637c10c
CrojasRD <cardlatam101@gmail.com>
2026-07-10 16:25:07 -0500

    Update admin-config.ts

--- lib/firebase/admin-config.ts
+++ lib/firebase/admin-config.ts
@@ -1,13 +1,19 @@
 import * as admin from 'firebase-admin';
+import { cert } from 'firebase-admin/app';
+
+let adminApp = admin.app();
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  adminApp = admin.initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
 }
 
-export const adminAuth = admin.auth();
-export const db = admin.firestore();
-export const storage = admin.storage();
+export const adminAuth = admin.auth(adminApp);
+export const db = admin.firestore(adminApp);
+export const storage = admin.storage(adminApp);
