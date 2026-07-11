35a4fad0109d39cac9c4d27bbd4e15fcf6165eb9
CrojasRD <cardlatam101@gmail.com>
2026-07-10 21:18:38 -0500

    Update admin-config.ts

--- lib/firebase/admin-config.ts
+++ lib/firebase/admin-config.ts
@@ -1,19 +1,33 @@
 import * as admin from 'firebase-admin';
 import { cert } from 'firebase-admin/app';
 
-let adminApp = admin.app();
+let adminApp: any;
 
-if (!admin.apps.length) {
-  adminApp = admin.initializeApp({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  });
+function initializeFirebase() {
+  if (!admin.apps.length && process.env.FIREBASE_PROJECT_ID) {
+    adminApp = admin.initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    });
+  }
+  return admin.app();
 }
 
-export const adminAuth = admin.auth(adminApp);
-export const db = admin.firestore(adminApp);
-export const storage = admin.storage(adminApp);
+export const adminAuth = () => {
+  const app = initializeFirebase();
+  return admin.auth(app);
+};
+
+export const db = () => {
+  const app = initializeFirebase();
+  return admin.firestore(app);
+};
+
+export const storage = () => {
+  const app = initializeFirebase();
+  return admin.storage(app);
+};
